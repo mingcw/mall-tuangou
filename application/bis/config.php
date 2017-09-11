dac824c1d74612b10844ade2b183feb41999dc52
mingcw <mingc2016@126.com>
2017-09-11 12:52:42 +0800

    修改商户API模块和API模块的配置文件

--- application/bis/config.php
+++ application/bis/config.php
@@ -1,11 +1,10 @@
 <?php
 //配置文件
 return [
-	// 'url_html_suffix' => '',
-
+	
 	// 商户模块session配置
     'session'                => [
-		'type'           => 'redis', // Redis驱动
+		'type'           => 'redis', // Redis驱动存储商户模块的所有Session
 		'prefix'         => 'bis',
 		'auto_start'     => true,
 
