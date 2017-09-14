82a61ae2904d27cc80ee4251f064e75749c1b3e4
mingcw <mingc2016@126.com>
2017-09-14 21:34:21 +0800

    细节优化

--- application/bis/controller/Login.php
+++ application/bis/controller/Login.php
@@ -24,7 +24,7 @@ class Login extends Controller
             
             $model = model('BisAccount');
             $where = ['username' => $data['username']];
-            $field = ['id', 'sort', 'create_time', 'update_time'];
+            $field = ['sort', 'create_time', 'update_time'];
             $bis = $model->where($where)->field($field, true)->find();
             if(!$bis || $bis->status != 1){
                 $this->error('用户名不存在或未通过审核');
